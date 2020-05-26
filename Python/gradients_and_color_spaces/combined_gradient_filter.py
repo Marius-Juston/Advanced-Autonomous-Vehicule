ceeaddf8e6ad2e1734efe4a7a0b6e3b70c6d46c9
Marius-Juston <Marius.juston@hotmail.fr>
2020-05-25 23:53:28 -0400

    Added gradient direction threshold

--- Python/gradients_and_color_spaces/combined_gradient_filter.py
+++ Python/gradients_and_color_spaces/combined_gradient_filter.py
@@ -29,7 +29,12 @@ def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
 def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi / 2)):
     # Calculate gradient direction
     # Apply threshold
-    return dir_binary
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    sobel_x = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobel_y = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    theta = np.arctan2(np.abs(sobel_y), np.abs(sobel_x))
+
+    return (theta >= thresh[0]) & (theta <= thresh[1])
 
 
 def combine(gradx, grady, magnitude, direction):
@@ -48,15 +53,14 @@ ksize = 3  # Choose a larger odd number to smooth gradient measurements
 
 # Apply each of the thresholding functions
 gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(20, 100))
-# dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi / 2))
 grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(30, 75))
 mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(30, 100))
-
+dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(np.pi / 4, np.pi / 2.5))
 
 cv2.imshow('Gradx', convert_to_image(gradx))
 cv2.imshow('Grady', convert_to_image(grady))
-# cv2.imshow('Direction', dir_binary)
 cv2.imshow('Magnitude', convert_to_image(mag_binary))
+cv2.imshow('Direction', convert_to_image(dir_binary))
 
 cv2.waitKey()
 cv2.destroyAllWindows()
