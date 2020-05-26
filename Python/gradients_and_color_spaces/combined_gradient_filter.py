34dae0059ab6f3606b7a0f78aaebffe23821edda
Marius-Juston <Marius.juston@hotmail.fr>
2020-05-25 23:43:42 -0400

    updated Sy threshold values

--- Python/gradients_and_color_spaces/combined_gradient_filter.py
+++ Python/gradients_and_color_spaces/combined_gradient_filter.py
@@ -48,9 +48,9 @@ ksize = 3  # Choose a larger odd number to smooth gradient measurements
 
 # Apply each of the thresholding functions
 gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(20, 100))
-grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(0, 255))
 # mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
 # dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi / 2))
+grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(30, 75))
 
 
 cv2.imshow('Gradx', convert_to_image(gradx))
