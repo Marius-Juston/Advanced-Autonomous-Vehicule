cb78652c3657dc5fcea9aebbcba9196ae05de12d
Marius-Juston <Marius.juston@hotmail.fr>
2020-05-28 16:50:07 -0400

    created dropout network

--- Python/deep_neural_networks/dropout.py
+++ Python/deep_neural_networks/dropout.py
@@ -20,3 +20,13 @@ biases = [
     tf.Variable(tf.zeros(2))]
 
 features = tf.Variable([[0.0, 2.0, 3.0, 4.0], [0.1, 0.2, 0.3, 0.4], [11.0, 12.0, 13.0, 14.0]])
+
+dropout_rate = .5
+
+hidden_layer = tf.add(tf.matmul(features, weights[0]), biases[0])
+hidden_layer = tf.nn.relu(hidden_layer)
+hidden_layer = tf.nn.dropout(hidden_layer, dropout_rate)
+
+output = tf.add(tf.matmul(hidden_layer, weights[1]), biases[1])
+
+tf.print(output)
