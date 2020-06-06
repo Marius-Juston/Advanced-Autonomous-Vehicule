4c4d845a8682f65712536d7023550ad3fe960b66
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-06 14:29:41 -0400

    started exteneted kalman filter jacobian calculation

---
+++ C++/CalculateJacobian/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include "Dense"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::cout;
+using std::endl;
+
+MatrixXd CalculateJacobian(const VectorXd &x_state);
+
+int main() {
+    /**
+     * Compute the Jacobian Matrix
+     */
+
+    // predicted state example
+    // px = 1, py = 2, vx = 0.2, vy = 0.4
+    VectorXd x_predicted(4);
+    x_predicted << 1, 2, 0.2, 0.4;
+
+    MatrixXd Hj = CalculateJacobian(x_predicted);
+
+    cout << "Hj:" << endl << Hj << endl;
+
+    return 0;
+}
+
+MatrixXd CalculateJacobian(const VectorXd &x_state) {
+
+    MatrixXd Hj(3, 4);
+    // recover state parameters
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+
+    // TODO: YOUR CODE HERE
+
+    // check division by zero
+
+    // compute the Jacobian matrix
+
+    return Hj;
+}
