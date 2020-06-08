6e57a4fdbf93b64abbb78659eb7ca242d5586b0d
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-07 23:47:41 -0400

    Used range based for loop, minor reformatting

--- C++/PriorInitialization/main.cpp
+++ C++/PriorInitialization/main.cpp
@@ -15,7 +15,7 @@ int main() {
   int map_size = 25;
 
   // initialize landmarks
-  vector<float> landmark_positions {5, 10, 20};
+  vector<float> landmark_positions{5, 10, 20};
 
   // initialize priors
   vector<float> priors = initialize_priors(map_size, landmark_positions,
@@ -38,12 +38,12 @@ vector<float> initialize_priors(int map_size, vector<float> landmark_positions,
 
   // set each landmark positon +/-1 to 1.0/9.0 (9 possible postions)
   float norm_term = landmark_positions.size() * (position_stdev * 2 + 1);
-  for (int i=0; i < landmark_positions.size(); ++i) {
-    for (float j=1; j <= position_stdev; ++j) {
-      priors.at(int(j+landmark_positions[i]+map_size)%map_size) += 1.0/norm_term;
-      priors.at(int(-j+landmark_positions[i]+map_size)%map_size) += 1.0/norm_term;
+  for (float landmark_position : landmark_positions) {
+    for (float j = 1; j <= position_stdev; ++j) {
+      priors.at(int(j + landmark_position + map_size) % map_size) += 1.0 / norm_term;
+      priors.at(int(-j + landmark_position + map_size) % map_size) += 1.0 / norm_term;
     }
-    priors.at(landmark_positions[i]) += 1.0/norm_term;
+    priors.at(landmark_position) += 1.0 / norm_term;
   }
 
   return priors;
