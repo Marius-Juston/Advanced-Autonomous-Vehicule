8bc0ff2849e6f010e32e851582b6a0ebb7b783db
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-09 19:14:44 -0400

    started particle weight calcualtion

---
+++ C++/ParticleWeight/main.cpp
@@ -0,0 +1,25 @@
+#include "Dense"
+#include <iostream>
+#include <cmath>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using namespace std;
+
+int main() {
+  double x_std = .3;
+  double y_std = .3;
+
+  double x = 6;
+  double y = 3;
+
+  double landmark_x = 5;
+  double landmark_y = 3;
+
+  double weight =
+      exp(-(pow(x - landmark_x, 2) / (2 * x_std) + pow(y - landmark_y, 2) / (2 * y_std))) / (2 * M_PI * x_std * y_std);
+
+  cout << weight << endl;
+
+  return 0;
+};
