cc4a75c8f59e49a15cf1bc0aa141051db0f79df2
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-10 22:12:42 -0400

    started naive bayes motion classifier

---
+++ C++/NaiveBayesMotionClassifier/classifier.cpp
@@ -0,0 +1,51 @@
+#include "classifier.h"
+#include <cmath>
+#include <string>
+#include <vector>
+
+using Eigen::ArrayXd;
+using std::string;
+using std::vector;
+
+// Initializes GNB
+GNB::GNB() {
+  /**
+   * TODO: Initialize GNB, if necessary. May depend on your implementation.
+   */
+
+}
+
+GNB::~GNB() {}
+
+void GNB::train(const vector<vector<double>> &data,
+                const vector<string> &labels) {
+  /**
+   * Trains the classifier with N data points and labels.
+   * @param data - array of N observations
+   *   - Each observation is a tuple with 4 values: s, d, s_dot and d_dot.
+   *   - Example : [[3.5, 0.1, 5.9, -0.02],
+   *                [8.0, -0.3, 3.0, 2.2],
+   *                 ...
+   *                ]
+   * @param labels - array of N labels
+   *   - Each label is one of "left", "keep", or "right".
+   *
+   * TODO: Implement the training function for your classifier.
+   */
+
+}
+
+string GNB::predict(const vector<double> &sample) {
+  /**
+   * Once trained, this method is called and expected to return 
+   *   a predicted behavior for the given observation.
+   * @param observation - a 4 tuple with s, d, s_dot, d_dot.
+   *   - Example: [3.5, 0.1, 8.5, -0.2]
+   * @output A label representing the best guess of the classifier. Can
+   *   be one of "left", "keep" or "right".
+   *
+   * TODO: Complete this function to return your classifier's prediction
+   */
+
+  return this->possible_labels[1];
+}
