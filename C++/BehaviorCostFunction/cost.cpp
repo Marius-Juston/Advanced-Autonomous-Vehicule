7e4092bba53d569d60059c5ef98d99b024c5cc84
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-11 11:43:27 -0400

    Created cost function

--- C++/BehaviorCostFunction/cost.cpp
+++ C++/BehaviorCostFunction/cost.cpp
@@ -7,10 +7,8 @@ double goal_distance_cost(int goal_lane, int intended_lane, int final_lane,
   //   and the distance of the final lane from the goal. The cost of being out 
   //   of the goal lane also becomes larger as the vehicle approaches the goal.
 
-  /**
-   * TODO: Replace cost = 0 with an appropriate cost function.
-   */
-  double cost = 0;
+  int delta_d = 2 * goal_lane - intended_lane - final_lane;
+  double cost = 1 - exp(-(std::abs(delta_d) / distance_to_goal));
 
   return cost;
 }
