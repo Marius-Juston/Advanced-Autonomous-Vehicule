97b0faa8f7e877e18eed2abc088868905f9aae93
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-11 11:54:06 -0400

    implemented speed change cost and renamed directory

--- C++/BehaviorSpeedChangeCostFunction/cost.cpp
+++ C++/BehaviorSpeedChangeCostFunction/cost.cpp
@@ -5,10 +5,9 @@ double inefficiency_cost(int target_speed, int intended_lane, int final_lane,
   // Cost becomes higher for trajectories with intended lane and final lane
   //   that have traffic slower than target_speed.
 
-  /**
-   * TODO: Replace cost = 0 with an appropriate cost function.
-   */
-  double cost = 0;
+  double speed_intended = lane_speeds[intended_lane];
+  double speed_final = lane_speeds[final_lane];
+  double cost = (2.0 * target_speed - speed_intended - speed_final) / target_speed;
 
   return cost;
 }
