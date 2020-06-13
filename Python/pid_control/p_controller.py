df5fe77252218b010395871ab07be0e9f7e3bc2c
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-13 12:19:53 -0400

    show only necessary graphs

--- Python/pid_control/p_controller.py
+++ Python/pid_control/p_controller.py
@@ -25,7 +25,6 @@ def run(robot, tau, n=100, speed=1.0):
 x_trajectory, y_trajectory = run(robot, 0.1)
 n = len(x_trajectory)
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
-ax1.plot(x_trajectory, y_trajectory, 'g', label='P controller')
-ax1.plot(x_trajectory, np.zeros(n), 'r', label='reference')
+plt.plot(x_trajectory, y_trajectory, 'g', label='P controller')
+plt.plot(x_trajectory, np.zeros(n), 'r', label='reference')
 plt.show()
--- Python/pid_control/pd_controller.py
+++ Python/pid_control/pd_controller.py
@@ -33,7 +33,6 @@ def run(robot, tau_p, tau_d, n=100, speed=1.0):
 x_trajectory, y_trajectory = run(robot, 0.2, 3.0)
 n = len(x_trajectory)
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
-ax1.plot(x_trajectory, y_trajectory, 'g', label='PD controller')
-ax1.plot(x_trajectory, np.zeros(n), 'r', label='reference')
+plt.plot(x_trajectory, y_trajectory, 'g', label='PD controller')
+plt.plot(x_trajectory, np.zeros(n), 'r', label='reference')
 plt.show()
