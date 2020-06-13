3b81a3e84e50a56d69d92a1934f851cdf052557f
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-13 12:31:17 -0400

    implemented pid controller

--- Python/pid_control/pid_controller.py
+++ Python/pid_control/pid_controller.py
@@ -11,6 +11,22 @@ def run(robot, tau_p, tau_d, tau_i, n=100, speed=1.0):
     x_trajectory = []
     y_trajectory = []
     # TODO: your code here
+
+    cummulation = 0
+    prev_cte = robot.y
+    dt = 1
+
+    for _ in range(n):
+        cte = robot.y
+        d_cte = (cte - prev_cte) / dt
+        cummulation += cte
+        prev_cte = cte
+
+        steer = - tau_p * cte - tau_d * d_cte - tau_i * cummulation
+        robot.move(steer, speed)
+        x_trajectory.append(robot.x)
+        y_trajectory.append(robot.y)
+
     return x_trajectory, y_trajectory
 
 
@@ -19,3 +35,4 @@ n = len(x_trajectory)
 
 plt.plot(x_trajectory, y_trajectory, 'g', label='PID controller')
 plt.plot(x_trajectory, np.zeros(n), 'r', label='reference')
+plt.show()
