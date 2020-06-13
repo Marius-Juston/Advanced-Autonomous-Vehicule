d4f0d6a976a3da7d5884c42ac7f8181ac5212b92
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-13 12:15:52 -0400

    implemented pd controller

---
+++ Python/pid_control/pd_controller.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pid_control.robot import Robot
+
+robot = Robot()
+robot.set(0, 1, 0)
+
+
+def run(robot, tau_p, tau_d, n=100, speed=1.0):
+    x_trajectory = []
+    y_trajectory = []
+    # TODO: your code here
+    prev_cte = -robot.y
+    dt = 1
+
+    for _ in range(n):
+        cte = -robot.y
+        d_cte = (cte - prev_cte) / dt
+
+        steer = tau_p * cte + tau_d * d_cte
+
+        robot.move(steer, speed)
+
+        x_trajectory.append(robot.x)
+        y_trajectory.append(robot.y)
+
+        prev_cte = cte
+
+    return x_trajectory, y_trajectory
+
+
+x_trajectory, y_trajectory = run(robot, 0.2, 3.0)
+n = len(x_trajectory)
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
+ax1.plot(x_trajectory, y_trajectory, 'g', label='PD controller')
+ax1.plot(x_trajectory, np.zeros(n), 'r', label='reference')
+plt.show()
