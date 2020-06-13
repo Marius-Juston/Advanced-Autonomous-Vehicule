2b436300e9c86b5dfdbf36195f373e3ebe38b5e9
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-13 12:43:13 -0400

    started twiddle algorithm

---
+++ Python/pid_control/twiddle.py
@@ -0,0 +1,53 @@
+def make_robot():
+    """
+    Resets the robot back to the initial position and drift.
+    You'll want to call this after you call `run`.
+    """
+    robot = Robot()
+    robot.set(0, 1, 0)
+    robot.set_steering_drift(10 / 180 * np.pi)
+    return robot
+
+
+# NOTE: We use params instead of tau_p, tau_d, tau_i
+def run(robot, params, n=100, speed=1.0):
+    x_trajectory = []
+    y_trajectory = []
+    err = 0
+    prev_cte = robot.y
+    int_cte = 0
+    for i in range(2 * n):
+        cte = robot.y
+        diff_cte = cte - prev_cte
+        int_cte += cte
+        prev_cte = cte
+        steer = -params[0] * cte - params[1] * diff_cte - params[2] * int_cte
+        robot.move(steer, speed)
+        x_trajectory.append(robot.x)
+        y_trajectory.append(robot.y)
+        if i >= n:
+            err += cte ** 2
+    return x_trajectory, y_trajectory, err / n
+
+
+# Make this tolerance bigger if you are timing out!
+def twiddle(tol=0.2):
+    # Don't forget to call `make_robot` before every call of `run`!
+    p = [0, 0, 0]
+    dp = [1, 1, 1]
+    robot = make_robot()
+    x_trajectory, y_trajectory, best_err = run(robot, p)
+    # TODO: twiddle loop here
+
+    return p, best_err
+
+
+params, err = twiddle()
+print("Final twiddle error = {}".format(err))
+robot = make_robot()
+x_trajectory, y_trajectory, err = run(robot, params)
+n = len(x_trajectory)
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
+ax1.plot(x_trajectory, y_trajectory, 'g', label='Twiddle PID controller')
+ax1.plot(x_trajectory, np.zeros(n), 'r', label='reference')
