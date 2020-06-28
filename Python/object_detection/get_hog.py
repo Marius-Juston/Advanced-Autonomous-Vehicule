4a0246653a15341001dbb5ae33ca14e316e019a1
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-27 22:12:36 -0400

    added skimge hog visualization

---
+++ Python/object_detection/get_hog.py
@@ -0,0 +1,43 @@
+import glob
+
+import cv2
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.feature import hog
+
+# Read in our vehicles
+car_images = glob.glob('vehicules/*.png')
+
+
+# Define a function to return HOG features and visualization
+# Features will always be the first element of the return
+# Image data will be returned as the second element if visualize= True
+# Otherwise there is no second return element
+
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=True,
+                     feature_vec=True):
+    return hog(img, orient, (pix_per_cell, pix_per_cell), (cell_per_block, cell_per_block), feature_vector=feature_vec,
+               visualize=vis)
+
+
+# Generate a random index to look at a car image
+ind = np.random.randint(0, len(car_images))
+# Read in the image
+image = mpimg.imread(car_images[ind])
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+# Call our function with vis=True to see an image output
+features, hog_image = get_hog_features(gray, orient=9,
+                                       pix_per_cell=8, cell_per_block=2,
+                                       vis=True, feature_vec=False)
+
+# Plot the examples
+fig = plt.figure()
+plt.subplot(121)
+plt.imshow(image, cmap='gray')
+plt.title('Example Car Image')
+plt.subplot(122)
+plt.imshow(hog_image, cmap='gray')
+plt.title('HOG Visualization')
+plt.show()
